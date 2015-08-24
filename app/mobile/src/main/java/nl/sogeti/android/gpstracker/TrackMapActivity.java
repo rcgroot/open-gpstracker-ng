9e63097ced2dd7b5dce775dcffb09821c20cbe4c
Rene de Groot <rene.de.groot@sogeti.nl>
2015-08-24 18:27:05 +0200

    Create new project wizard

---
+++ app/mobile/src/main/java/nl/sogeti/android/gpstracker/TrackMapActivity.java
@@ -0,0 +1,38 @@
+package nl.sogeti.android.gpstracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class TrackMapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_track_map);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_track_map, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/mobile/src/main/java/nl/sogeti/android/gpstracker/TrackMapActivityFragment.java
@@ -0,0 +1,22 @@
+package nl.sogeti.android.gpstracker;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class TrackMapActivityFragment extends Fragment {
+
+    public TrackMapActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_track_map, container, false);
+    }
+}
