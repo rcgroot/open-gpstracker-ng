a4f477935b4c7f8fc8d83afccfae5cec74b0fb14
Rene de Groot <rcgroot@gmail.com>
2016-01-09 10:47:59 +0100

    Shove a Google Maps v2 map inthere

--- studio/app/src/main/java/nl/sogeti/android/gpstracker/map/TrackMapActivityFragment.java
+++ studio/app/src/main/java/nl/sogeti/android/gpstracker/map/TrackMapActivityFragment.java
@@ -1,21 +1,21 @@
 package nl.sogeti.android.gpstracker.map;
 
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import nl.sogeti.android.gpstracker.v2.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class TrackMapActivityFragment extends Fragment {
+public class TrackMapActivityFragment extends MapFragment implements OnMapReadyCallback {
 
-    public TrackMapActivityFragment() {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getMapAsync(this);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_track_map, container, false);
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
