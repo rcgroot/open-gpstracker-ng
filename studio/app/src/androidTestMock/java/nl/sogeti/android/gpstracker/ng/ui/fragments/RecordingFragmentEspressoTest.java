814f233300355f789b2cf5974fa2de108013521c
Rene de Groot <rcgroot@gmail.com>
2016-10-01 10:34:24 +0200

    Visibility of the recording info pane

--- studio/app/src/androidTestMock/java/nl/sogeti/android/gpstracker/ng/ui/fragments/RecordingFragmentEspressoTest.java
+++ studio/app/src/androidTestMock/java/nl/sogeti/android/gpstracker/ng/ui/fragments/RecordingFragmentEspressoTest.java
@@ -1,3 +1,31 @@
+/*------------------------------------------------------------------------------
+ **     Ident: Sogeti Smart Mobile Solutions
+ **    Author: rene
+ ** Copyright: (c) 2016 Sogeti Nederland B.V. All Rights Reserved.
+ **------------------------------------------------------------------------------
+ ** Sogeti Nederland B.V.            |  No part of this file may be reproduced
+ ** Distributed Software Engineering |  or transmitted in any form or by any
+ ** Lange Dreef 17                   |  means, electronic or mechanical, for the
+ ** 4131 NJ Vianen                   |  purpose, without the express written
+ ** The Netherlands                  |  permission of the copyright holder.
+ *------------------------------------------------------------------------------
+ *
+ *   This file is part of OpenGPSTracker.
+ *
+ *   OpenGPSTracker is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   OpenGPSTracker is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with OpenGPSTracker.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package nl.sogeti.android.gpstracker.ng.ui.fragments;
 
 import org.junit.After;
@@ -6,6 +34,7 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import nl.sogeti.android.gpstracker.ng.recording.RecordingFragment;
+import nl.sogeti.android.gpstracker.ng.util.EspressoTestMatchers;
 import nl.sogeti.android.gpstracker.ng.util.FragmentTestRule;
 import nl.sogeti.android.gpstracker.ng.util.MockServiceManager;
 import nl.sogeti.android.gpstracker.v2.R;
@@ -39,6 +68,42 @@ public class RecordingFragmentEspressoTest {
 
     @Test
     public void testStartUp() {
+        // Verify
+        onView(withId(R.id.fragment_recording_container)).check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void testVisibleWhenStarted() {
+        // Execute
+        mockServiceManager.startGPSLogging(sut.getActivity(), null);
+
+        // Verify
+        onView(withId(R.id.fragment_recording_container)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testVisibleWhenPaused() {
+        // Execute
+        mockServiceManager.pauseGPSLogging(sut.getActivity());
+
+        // Verify
+        onView(withId(R.id.fragment_recording_container)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testVisibleWhenResumed() {
+        // Execute
+        mockServiceManager.resumeGPSLogging(sut.getActivity());
+
+        // Verify
+        onView(withId(R.id.fragment_recording_container)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testVisibleWhenStopped() {
+        // Execute
+        mockServiceManager.stopGPSLogging(sut.getActivity());
+
         // Verify
         onView(withId(R.id.fragment_recording_container)).check(matches(not(isDisplayed())));
     }
