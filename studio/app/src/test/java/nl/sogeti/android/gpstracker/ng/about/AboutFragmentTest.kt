2a8dbc9098cd9704693fe7c026e9ad141fb3aee0
Rene de Groot <rcgroot@gmail.com>
2017-03-16 20:40:46 +0100

    could be any hash

--- studio/app/src/test/java/nl/sogeti/android/gpstracker/ng/about/AboutFragmentTest.kt
+++ studio/app/src/test/java/nl/sogeti/android/gpstracker/ng/about/AboutFragmentTest.kt
@@ -2,6 +2,7 @@ package nl.sogeti.android.gpstracker.ng.about
 
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.Matchers.greaterThan
+import org.hamcrest.Matchers.notNullValue
 import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Test
@@ -21,7 +22,7 @@ class AboutFragmentTest {
         val model = sut.model
         // Assert
         assertThat(model.buildNumber, `is`(greaterThan(0)))
-        assertThat(model.gitHash, `is`("null"))
+        assertThat(model.gitHash, `is`(notNullValue()))
         assertThat(model.url, `is`("file:///android_asset/about.html"))
         assertThat(model.version, `is`("2.0.0-SNAPSHOT"))
     }
