17edfd1641278354cd37cdac29593b4b7e546720
Rene de Groot <rcgroot@gmail.com>
2017-03-16 20:37:54 +0100

    hamcrest, not mockito matchers there

--- studio/app/src/test/java/nl/sogeti/android/gpstracker/ng/about/AboutFragmentTest.kt
+++ studio/app/src/test/java/nl/sogeti/android/gpstracker/ng/about/AboutFragmentTest.kt
@@ -1,10 +1,10 @@
 package nl.sogeti.android.gpstracker.ng.about
 
 import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.Matchers.greaterThan
 import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Test
-import org.mockito.ArgumentMatchers.anyInt
 
 class AboutFragmentTest {
 
@@ -20,7 +20,7 @@ class AboutFragmentTest {
         // Act
         val model = sut.model
         // Assert
-        assertThat(model.buildNumber, `is`(anyInt()))
+        assertThat(model.buildNumber, `is`(greaterThan(0)))
         assertThat(model.gitHash, `is`("null"))
         assertThat(model.url, `is`("file:///android_asset/about.html"))
         assertThat(model.version, `is`("2.0.0-SNAPSHOT"))
