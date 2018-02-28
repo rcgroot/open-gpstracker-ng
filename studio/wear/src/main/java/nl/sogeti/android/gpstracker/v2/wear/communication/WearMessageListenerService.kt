f7b902db88117b5f3fd2589960c8c766e4261caa
Rene de Groot <rcgroot@gmail.com>
2018-02-28 21:43:45 +0100

    reduce logcat warnings

--- studio/wear/src/main/java/nl/sogeti/android/gpstracker/v2/wear/communication/WearMessageListenerService.kt
+++ studio/wear/src/main/java/nl/sogeti/android/gpstracker/v2/wear/communication/WearMessageListenerService.kt
@@ -1,5 +1,6 @@
 package nl.sogeti.android.gpstracker.v2.wear.communication
 
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import nl.sogeti.android.gpstracker.v2.sharedwear.messaging.MessageListenerService
 import nl.sogeti.android.gpstracker.v2.sharedwear.messaging.StatisticsMessage
 import nl.sogeti.android.gpstracker.v2.sharedwear.messaging.StatusMessage
@@ -9,11 +10,13 @@ class WearMessageListenerService : MessageListenerService() {
 
     override fun updateStatus(status: StatusMessage) {
         val intent = ControlActivity.createIntent(this, status)
+        intent.addFlags(FLAG_ACTIVITY_NEW_TASK)
         startActivity(intent)
     }
 
     override fun updateStatistics(statistics: StatisticsMessage) {
         val intent = ControlActivity.createIntent(this, statistics)
+        intent.addFlags(FLAG_ACTIVITY_NEW_TASK)
         startActivity(intent)
     }
 }
